60f9c2160a55712ea8cf7ce59b1fd345bffcd73f
Bramble Burrows <brambledeer@gmail.com>
2024-10-04 09:49:36 -0700

    Added new styling as well as fixing bugs with search features

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
+//Take search box input and display in header
 function search() {
     const result = document.getElementById('result');
     const username = document.getElementById('username').value;
     result.innerHTML = username;   
 }
 
-
+//Send serach box input to console 
 function debug() {
-    const shit = result.textContent;
-    console.log(shit);
+    const debug = result.textContent;
+    console.log(debug);
 }
