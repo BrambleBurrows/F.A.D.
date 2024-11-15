fea44ad897975e9481da45d2adcb95261df62344
Bramble Deer <brambledeer@gmail.com>
2024-11-15 01:31:21 -0800

    Moved to Desktop

--- includes/loginmanager.php
+++ includes/loginmanager.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
+$servername = "192.168.1.195:3306";
+$username = "remote";
 $password = "g^&g1e2D25[pm6y<o?/T]7+5AI7bNN";
 $dbname = "adminlogins";
 
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Password FROM logins WHERE Name='Logan Hogue';";
+$sql = "SELECT Pass FROM logins WHERE Name='Logan Hogue';";
 $result = $conn->query($sql);
 
 
--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -27,7 +27,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '192.168.1.195:3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
